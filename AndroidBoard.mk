61fbcc2f2836682ee61f7c35702d8e4b2cdc1f11
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2013-02-01 19:41:40 +0700

    major update

--- AndroidBoard.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-# include the non-open-source counterpart to this file
--include vendor/huawei/u8510/AndroidBoardVendor.mk
